f65e1f25fcb117c1757310bba7e6599250108e21
alexander <alexander.regalado@ikasle.egibide.org>
2022-04-26 20:27:38 +0200

    Recicler View 1 - libreria gradle

--- app/src/main/java/com/example/kotlin_proyecto_alex_egoitz/Adaptador.kt
+++ app/src/main/java/com/example/kotlin_proyecto_alex_egoitz/Adaptador.kt
@@ -1,4 +1,9 @@
 package com.example.kotlin_proyecto_alex_egoitz
 
-class Adaptador {
-}
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Adaptador{}
