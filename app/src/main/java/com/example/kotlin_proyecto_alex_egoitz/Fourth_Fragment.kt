9f7d27f12eede8ecb4f57fcf1246f761958132aa
egoitz <egoitz.perezdearrilucea@ikasle.egibide.org>
2022-05-03 21:21:05 +0200

    Fourth fragment (el spiner no va)

--- app/src/main/java/com/example/kotlin_proyecto_alex_egoitz/Fourth_Fragment.kt
+++ app/src/main/java/com/example/kotlin_proyecto_alex_egoitz/Fourth_Fragment.kt
@@ -1,12 +1,15 @@
 package com.example.kotlin_proyecto_alex_egoitz
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import com.example.kotlin_proyecto_alex_egoitz.databinding.FourthFragmentBinding
 import com.example.kotlin_proyecto_alex_egoitz.databinding.ThirdFragmentBinding
+
 
 class Fourth_Fragment : Fragment() {
     private var _binding: FourthFragmentBinding? = null
@@ -15,13 +18,19 @@ class Fourth_Fragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+
+
+
     override fun onCreateView(
+
+
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FourthFragmentBinding.bind().inflate(inflater, container, false)
         return binding.root
+
 
     }
 
@@ -34,4 +43,6 @@ class Fourth_Fragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
