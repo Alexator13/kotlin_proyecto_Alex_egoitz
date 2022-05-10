7f4e18818cb207c86025c4bd9515afc78204726a
egoitz <egoitz.perezdearrilucea@ikasle.egibide.org>
2022-05-10 19:40:39 +0200

    Spiner acabado (Hay que probarlo)

--- app/src/main/java/com/example/kotlin_proyecto_alex_egoitz/Fourth_Fragment.kt
+++ app/src/main/java/com/example/kotlin_proyecto_alex_egoitz/Fourth_Fragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import com.example.kotlin_proyecto_alex_egoitz.databinding.FourthFragmentBinding
 import com.example.kotlin_proyecto_alex_egoitz.databinding.ThirdFragmentBinding
@@ -37,12 +39,36 @@ class Fourth_Fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val spinner: Spinner = binding.spinnerDestinos
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            activity as MainActivity,
+            R.array.destinos,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+     fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+     fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+    }
+
 
 
 }
